948be8a56d4601aec7fa4ea1072995a731d50048
vothuclong <vothuclong@gmail.com>
2021-07-02 11:39:22 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        TestCircle circle = new TestCircle();
-        System.out.println("The circle's radius: " + circle.getRadius());
-        System.out.println("The circle's color: " + circle.getArea());
+
     }
 }
