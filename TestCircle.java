f52aa1d90194aa25b5ac7cf5c0afd54cdb8a9b17
vothuclong <vothuclong@gmail.com>
2021-07-02 09:34:37 +0700

    Create TestCircle.java

---
+++ TestCircle.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class TestCircle {
+    private double radius = 1.0;
+    private String color = "red";
+    TestCircle() {
+    }
+    TestCircle(double r) {
+        this.radius = r;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public String getArea(){
+        return color;
+    }
+}
