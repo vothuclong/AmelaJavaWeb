d7989a7b62096aadb558f5e65ed31fd22011c3f2
vothuclong <vothuclong@gmail.com>
2021-07-02 09:34:34 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        TestCircle circle = new TestCircle();
+        System.out.println("The circle's radius: " + circle.getRadius());
+        System.out.println("The circle's color: " + circle.getArea());
+    }
+}
