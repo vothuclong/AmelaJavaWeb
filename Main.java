08c6646d4e1b11733d19a322b9d2680096aab1b6
vothuclong <vothuclong@gmail.com>
2021-07-04 20:10:30 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,26 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
-    public static void main(String[] args) {
-        ComparableCircle[] circles = new ComparableCircle[3];
-        circles[0] = new ComparableCircle(3.6);
-        circles[1] = new ComparableCircle();
-        circles[2] = new ComparableCircle(3.5, "indigo", false);
+        public static void main(String[] args) {
+            Circle[] circles = new Circle[3];
+            circles[0] = new Circle(3.6);
+            circles[1] = new Circle();
+            circles[2] = new Circle(3.5, "indigo", false);
 
-        System.out.println("Pre-sorted: ");
-        for (ComparableCircle circle : circles) {
-            System.out.println(circle);
+            System.out.println("Pre-sorted:");
+            for (Circle circle : circles) {
+                System.out.println(circle);
+            }
+
+            Comparator circleComparator = new CircleComparator();
+            Arrays.sort(circles, circleComparator);
+
+            System.out.println("After-sorted:");
+            for (Circle circle : circles) {
+                System.out.println(circle);
+            }
         }
-
-        Arrays.sort(circles);
-
-        System.out.println("After-sorted: ");
-        for (ComparableCircle circle : circles) {
-            System.out.println(circle);
-        }
-    }
 }
