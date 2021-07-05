c9bfaaed5f6092a28827ca40ddcae697691c159e
vothuclong <vothuclong@gmail.com>
2021-07-05 11:27:58 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,39 +2,15 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1. Stack of Strings");
-        stackOfIStrings();
-        System.out.println("\n2. Stack of Integers");
-        stackOfIntegers();
-    }
-
-    private static void stackOfIStrings() {
-        MyGenericStack<String> stack = new MyGenericStack();
-        stack.push("Five");
-        stack.push("Four");
-        stack.push("Three");
-        stack.push("Two");
-        stack.push("One");
-        System.out.println("1.1. Size of stack after push operations: " + stack.size());
-        System.out.printf("1.2. Pop elements from stack: ");
-        while (!stack.isEmpty()) {
-            System.out.printf(" %s", stack.pop());
-        }
-        System.out.println("\n1.3. Size of stack after pop operations: " + stack.size());
-    }
-
-    private static void stackOfIntegers() {
-        MyGenericStack<Integer> stack = new MyGenericStack();
-        stack.push(5);
-        stack.push(4);
-        stack.push(3);
-        stack.push(2);
-        stack.push(1);
-        System.out.println("2.1. Size of stack after push operations: " + stack.size());
-        System.out.printf("2.2. Pop elements from stack : ");
-        while (!stack.isEmpty()) {
-            System.out.printf(" %d", stack.pop());
-        }
-        System.out.println("\n2.3 Size of stack after pop operations : " + stack.size());
+        MyLinkedListQueue queue = new MyLinkedListQueue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.dequeue();
+        System.out.println("Dequeued item is " + queue.dequeue().key);
+        queue.dequeue();
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+        System.out.println("Dequeued item is " + queue.dequeue().key);
     }
 }
