0657877e0d3f7aaaa443733f0da11aa5ca4a3408
vothuclong <vothuclong@gmail.com>
2021-07-06 08:33:26 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,91 @@
 package com.company;
 
-public class Cylinder {
-    public static double getVolume(int radius, int height){
-        double baseArea = getBaseArea(radius);
-        double perimeter = getPerimeter(radius);
-        double volume = perimeter * height + 2 * baseArea;
-        return volume;
+public class Main {
+    static String PLAYER1_ADVANTAGE = "Advantage Player 1";
+    static String PLAYER2_ADVANTAGE = "Advantage Player 2";
+    static String PLAYER1_WIN = "Player 1 WIN!";
+    static String PLAYER2_WIN = "Player 2 WIN!";
+    static final int LOVE = 0;
+    static final int FIFTEEN = 1;
+    static final int THIRTY = 2;
+    static final int FORTY = 3;
+
+    public static String getScore(String player1Name, String player2Name, int player1Score, int player2Score) {
+        boolean isDeuce = player1Score == player2Score;
+        String score = "";
+
+        if (isDeuce) {
+            score = deuceScore(player1Score);
+        }
+        else if (player1Score >= 4 || player2Score >= 4) {
+            int minusResult = player1Score - player2Score;
+            if (minusResult == 1) {
+                score = PLAYER1_ADVANTAGE;
+            } else if (minusResult == -1) {
+                score = PLAYER2_ADVANTAGE;
+            } else if (minusResult >= 2) {
+                score = PLAYER1_WIN;
+            } else {
+                score = PLAYER2_WIN;
+            }
+        }
+        else {
+            score = scoreBoard(player1Score, player2Score);
+        }
+        return score;
     }
 
-    private static double getPerimeter(int radius) {
-        return 2 * Math.PI  * radius;
+    public static String deuceScore(int playerScore) {
+        String score = "";
+
+        switch (playerScore) {
+            case LOVE:
+                score = "Love-All";
+                break;
+            case FIFTEEN:
+                score = "Fifteen-All";
+                break;
+            case THIRTY:
+                score = "Thirty-All";
+                break;
+            case FORTY:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
+        }
+        return score;
     }
 
-    private static double getBaseArea(int radius) {
-        return Math.PI * radius * radius;
+    public static String scoreBoard(int player1Score, int player2Score) {
+        int tempScore = 0;
+        String score = "";
+
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) {
+                tempScore = player1Score;
+            } else {
+                score += "-";
+                tempScore = player2Score;
+            }
+
+            switch(tempScore) {
+                case LOVE:
+                    score += "Love";
+                    break;
+                case FIFTEEN:
+                    score += "Fifteen";
+                    break;
+                case THIRTY:
+                    score += "Thirty";
+                    break;
+                case FORTY:
+                    score += "Forty";
+                    break;
+            }
+        }
+        return score;
     }
 }
