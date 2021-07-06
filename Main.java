aa31cd57139e551703d48ced97d4beb0e8807a42
vothuclong <vothuclong@gmail.com>
2021-07-06 08:27:53 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,18 @@
 package com.company;
 
-public class Main {
-    public static void main(String[] args){
-        Shape[] shapes = new Shape[5];
-        shapes[0] = new Square(1.2);
-        shapes[1] = new Circle(2.5);
-        shapes[2] = new Rectangle(2.2, 5.5);
+public class Cylinder {
+    public static double getVolume(int radius, int height){
+        double baseArea = getBaseArea(radius);
+        double perimeter = getPerimeter(radius);
+        double volume = perimeter * height + 2 * baseArea;
+        return volume;
+    }
 
-        for (Shape shape : shapes){
-            System.out.println(shape.toString());
-            if (shape instanceof Colorable){
-                ((Colorable)shape).howToColor();
-            }
-        }
+    private static double getPerimeter(int radius) {
+        return 2 * Math.PI  * radius;
+    }
+
+    private static double getBaseArea(int radius) {
+        return Math.PI * radius * radius;
     }
 }
